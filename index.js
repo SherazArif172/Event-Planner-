45786a653798f0bca0755cbe08cc82af79afb6a8
Sheraz Arif <sherazarifofficial@gmail.com>
2025-09-26 16:33:50 +0500

    added

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ import express from "express";
 import startServer from "./server.js";
 import eventsRouter from "./src/Routes/events.js";
 
-
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3001",              
+      "https://event-planner-4ezh.onrender.com", 
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/events", eventsRouter);
 
-
-
 startServer(app);
-
